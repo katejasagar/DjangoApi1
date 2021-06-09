e743fccaca47da896005d445be3de07295115030
Sagar Kateja <katejasagar@gmail.com>
2021-06-10 02:45:35 +0530

    created modelPost, searialises data to send Json file

--- core/views.py
+++ core/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-from django.http import JsonResponse #to return jSon responce
+from django.http import JsonResponse
+from rest_framework import serializers #to return jSon responce
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import PostSerializer
+from .models import Post
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
-        data = {
-        'name': 'Sagar',
-        'age': 25}
-        return Response(data)
+        qs = Post.objects.all()
+        serializer = PostSerializer(qs, many = True)
+        return Response(serializer.data)
     
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
