c044c146e8a659e9d58469ee13cf31db7c9d672a
Sagar Kateja <katejasagar@gmail.com>
2021-06-14 01:26:09 +0530

    exploring views

--- core/views.py
+++ core/views.py
@@ -4,23 +4,54 @@ from rest_framework import serializers #to return jSon responce
 
 from rest_framework.permissions import IsAuthenticated # allowany, you can allow anyone to use the api // Isauthenticated , authentication is required
 from rest_framework.views import APIView
+from rest_framework import mixins # mixins are the containers that provide the functionality like login req
 from rest_framework.response import Response
+from rest_framework import generics
 from .serializers import PostSerializer
 from .models import Post
 
-class TestView(APIView):
+class PostView(mixins.ListModelMixin, mixins.CreateModelMixin,  generics.GenericAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
 
-    permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
-        qs = Post.objects.all()
-        serializer = PostSerializer(qs, many = True)
-        return Response(serializer.data)
-    
+        return self.list( request,*args, **kwargs)
+
     def post(self, request, *args, **kwargs):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
+        return self.create( request, *args, **kwargs)
+
+
+class PostCreateView(mixins.ListModelMixin, generics.CreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    #we dont need to call post method as CreateAPIView is already doing it, it only handles post method
+
+    def get(self, request, *args, **kwargs):
+        return self.list( request,*args, **kwargs)
+
+
+class PostListCreateView(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+
+
+
+
+# class TestView(APIView):
+
+#     permission_classes = (IsAuthenticated,)
+
+#     def get(self, request, *args, **kwargs):
+#         qs = Post.objects.all()
+#         serializer = PostSerializer(qs, many = True)
+#         return Response(serializer.data)
+    
+#     def post(self, request, *args, **kwargs):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django import urls
 from django.contrib import admin
 from django.urls import path, include
-from core.views import TestView
+from core.views import PostView, PostCreateView, PostListCreateView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path( '', TestView.as_view(), name = 'test' ),
+    path( '', PostView.as_view(), name = 'test' ),
+    path( 'create/', PostCreateView.as_view(), name = 'create' ),
+    path( 'list-create/', PostListCreateView.as_view(), name = 'list-create' ),
     path('rest-auth/', include('rest_auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', obtain_auth_token, name='obtain-token'),
